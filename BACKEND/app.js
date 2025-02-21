c650e356cc9a1638d021a46e7f4525d0f165d095
Hitesh-Thakur607 <157588459+Hitesh-Thakur607@users.noreply.github.com>
2025-02-21 20:05:17 +0530

    Delete BACKEND directory

--- BACKEND/app.js
+++
@@ -1,5 +0,0 @@
-import express from "express";
-const app=express();
-app.listen(3069,()=>{
-    console.log("server is runnings on port 3069");
-}) 
--- BACKEND/data/database.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-export const connectdb=()=>{
-mongoose
-  .connect(process.env .MONGO_URI, {
-    dbName: "CHATHUB",
-  })
-  .then((c) => console.log('Database Connected TO ',c.connection.host))
-  .catch((e) => console.log(e));
-}
