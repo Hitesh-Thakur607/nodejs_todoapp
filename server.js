51e57065dd88cd3c161f99308784c9e2c6a3c449
Hitesh-Thakur607 <157588459+Hitesh-Thakur607@users.noreply.github.com>
2025-03-31 18:20:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -30,29 +30,70 @@
 // const PORT = process.env.PORT || 4000;
 // app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-import express from "express";
+// import express from "express";
+// import dotnev from "dotenv";
+// import morgan from "morgan";
+// import cookieParser from "cookie-parser";
+// import cors from "cors";
+// import mongoose from "mongoose";
+// import userRouter from "./routes/user.js"
+// import taskrouter from "./routes/task.js"
+// import {connectdb} from "./data/database.js"
+// import bodyParser from 'body-parser';
+
+// const app = express();
+// app.use(cookieParser());
+// app.use(morgan("dev"));
+// app.use(bodyParser.json());
+
+// app.use(cors({
+//   origin: process.env.FRONTEND_URL || "http://localhost:5173",
+//   methods: ["GET", "POST", "PUT", "DELETE"],
+//   credentials: true,
+//   allowedHeaders: ["Content-Type", "Authorization"],
+// }));
+
+// dotnev.config({
+//   path:"./data/config.env"
+// })
+// console.log(process.env.PORT);
+
+// connectdb(); 
+
+// app.use(express.json());
+
+// app.use("/api/v1/users", userRouter);
+// app.use("/api/v1/task", taskrouter);
+
+// app.get("/", (req, res) => {
+//   res.send("nice working");
+// });
+
+// const PORT = process.env.PORT || 4000;
+
+// app.listen(process.env.PORT, () => {
+//   console.log("server is runnings on port ",process.env.PORT);
+// });
 import dotnev from "dotenv";
 import morgan from "morgan";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-import mongoose from "mongoose";
+// import mongoose from "mongoose";
+// import { string } from "prop-types";
 import userRouter from "./routes/user.js"
 import taskrouter from "./routes/task.js"
 import {connectdb} from "./data/database.js"
 import bodyParser from 'body-parser';
-
+import express from "express";
 const app = express();
 app.use(cookieParser());
 app.use(morgan("dev"));
 app.use(bodyParser.json());
-
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  credentials: true,
-  allowedHeaders: ["Content-Type", "Authorization"],
+  origin: [process.env.FRONTEND_URL],
+  methods:["GET","PUT","PUT","DELETE"],
+  credentials:true,
 }));
-
 dotnev.config({
   path:"./data/config.env"
 })
