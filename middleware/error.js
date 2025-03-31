9b78fc5425b70e12423d9449af827138ba549db3
Hitesh-Thakur607 <157588459+Hitesh-Thakur607@users.noreply.github.com>
2025-03-31 17:31:58 +0530

    Update error.js

--- middleware/error.js
+++ middleware/error.js
@@ -4,12 +4,20 @@ constructor(message,statuscode){
     this.statusCode=statuscode;
 }
 }
-export const err=((err,req,res,next)=>{
-    err.message=err.message||"internal server error";
-    err.statuscode=err.statuscode ||500;
-    return res.status(404).json({
-        sucess:false,
-        message:err.message
-    })
-})
+export const err = (err, req, res, next) => {
+  console.error("Error:", err);
+  return res.status(err.statusCode || 500).json({
+    success: false,
+    message: err.message || "Internal Server Error"
+  });
+};
+
+// export const err=((err,req,res,next)=>{
+//     err.message=err.message||"internal server error";
+//     err.statuscode=err.statuscode ||500;
+//     return res.status(404).json({
+//         sucess:false,
+//         message:err.message
+//     })
+// })
 export default errorhandler
